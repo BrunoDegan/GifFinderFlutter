2073046fc024813be661b4043964d88978d0c4f6
Bruno Degan <bmdegan@gmail.com>
2019-11-10 21:27:15 -0400

    GIF list application made on Flutter for android and iOS devices

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:gif_finder_flutter/ui/home_page.dart';
+
+void main() {
+    runApp(MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(hintColor: Colors.white),
+  ));
+}
