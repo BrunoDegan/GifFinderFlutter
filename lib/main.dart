75928e9dc9b6e0c04ca89a429048f2c08eb9b012
Bruno Degan <bmdegan@gmail.com>
2021-02-08 02:00:47 -0400

    Libs upgrade

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:gif_finder_flutter/ui/home_page.dart';
 
 void main() {
-    runApp(MaterialApp(
-      home: HomePage(),
-      theme: ThemeData(hintColor: Colors.white),
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
   ));
 }
